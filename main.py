85cb85e21f9824018e18eeea76da4caf2a790f33
Aarin06 <aarinjasikumar@gmail.com>
2025-01-30 15:19:48 -0500

    created user interface

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ class Wordle:
 
     MAX_TURNS = 5
 
+    WINNER = "You got it! Amazing!"
+
+    TRY_AGAIN = "Wrong guess!"
+
+    GAME_OVER = "You're out of turns, game over!"
+
+    INPUT_STRING = "Please enter your first guess: "
+
     def __init__(self):
         self.turns = 0
         self.solution = self.dictionary.getRandomWord()
@@ -25,3 +33,25 @@ class Wordle:
 
     def hint(self, word):
         pass
+
+
+def main():
+
+    wordle = Wordle()
+    print(f"Welcome to Word Guess! You have {wordle.MAX_TURNS} turns to guess the word. ")
+
+
+    while wordle.turns < wordle.MAX_TURNS:
+        attempt = input(wordle.INPUT_STRING).lower()
+
+        result = wordle.guess(attempt)
+
+        if result:
+            print(wordle.WINNER)
+            break
+        
+        print(wordle.TRY_AGAIN)
+    
+
+    if wordle.turns >= wordle.MAX_TURNS:
+        print(wordle.GAME_OVER)
